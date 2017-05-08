13fefd3b70115da820b6dd8cbf512de4575e3aae
YangJing96 <yangjing9611@foxmail.com>
2017-05-08 08:58:15 +0800

    Action:alter Description:修改BaseFrameFragment基类中初始化Presenter层方法，防止“内存重启”时空指针异常

--- commonLib/src/main/java/com/jkb/commonlib/base/ui/BaseFrameFragment.java
+++ commonLib/src/main/java/com/jkb/commonlib/base/ui/BaseFrameFragment.java
@@ -3,6 +3,9 @@ package com.jkb.commonlib.base.ui;
 import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.jkb.commonlib.base.frame.BasePresenter;
 import com.jkb.commonlib.base.ui.action.frame.IMVPAction;
@@ -17,10 +20,13 @@ public abstract class BaseFrameFragment<P extends BasePresenter, VM extends View
 
     protected BasePresenter mPresenter;
 
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle
+            savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
         initPresenter();
+        return view;
     }
 
     @Override
